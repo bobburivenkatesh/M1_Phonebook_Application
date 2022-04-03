24b196b71dfc4e372bccb52af34fd432bbdca144
bobburivenkatesh <102639235+bobburivenkatesh@users.noreply.github.com>
2022-04-03 18:38:42 +0530

    Add files via upload

---
+++ 3_Implementation/header/header.c
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Person
+{
+};
+void appendData();
+void showAllData();
+void findData();
+void deleteData();
+void displayMenu();
+
+
